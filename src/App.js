b7f16530805b96d157c3c71bd3fc5335e08632eb
abdulbasit-web <abdullbasit99salah@gmail.com>
2020-09-27 09:40:04 +0300

    setting up router adn add 404 page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React from 'react'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import NewPlace from './places/pages/NewPlace'
+import NotFound from './shared/components/NotFound'
+import Users from './users/pages/Users'
 
 function App() {
-  return <h1>Let's start!</h1>;
+  return (
+    <Router>
+      <Switch>
+        <Route path='/' exact>
+          <Users />
+        </Route>
+        <Route path='/places/new'>
+          <NewPlace />
+        </Route>
+        <Route>
+          <NotFound />
+        </Route>
+      </Switch>
+    </Router>
+  )
 }
 
-export default App;
+export default App
