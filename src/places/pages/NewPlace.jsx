399bdeb0c65cb43146bbfed68bfe03f36a1b090d
abdulbasit-web <abdullbasit99salah@gmail.com>
2020-10-19 01:12:42 +0300

    make mircle

--- src/places/pages/NewPlace.jsx
+++ src/places/pages/NewPlace.jsx
@@ -1,22 +1,59 @@
-import React, {useCallback} from 'react'
+import React, {useCallback, useReducer} from 'react'
 
+import Button from '../../shared/components/FormElements/Button'
 import Input from '../../shared/components/FormElements/Input'
 import {VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE} from '../../shared/util/validators'
 import './NewPlace.css'
 
+function reducer(state, action) {
+  switch (action.type) {
+    case 'INPUT_CHANGE':
+      let formValid = true
+      for (const inputId in state.inputs) {
+        if (inputId === action.inputId) formValid = formValid && action.isValid
+        else formValid = formValid && state.inputs[inputId].isValid
+      }
+
+      return {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          [action.inputId]: {value: action.value, isValid: action.isValid},
+        },
+        isValid: formValid,
+      }
+    default:
+      return state
+  }
+}
+
 function NewPlace() {
+  const [formState, dispatch] = useReducer(reducer, {
+    inputs: {
+      title: {
+        value: '',
+        isValid: false,
+      },
+      descrption: {
+        value: '',
+        isValid: false,
+      },
+    },
+    isValid: false,
+  })
+
   //when ever the compenent render again it create  a new titleInputHandler
   //to prevent this we use useCallback, the function will be stored and
   //reused again
-  const titleInputHandler = useCallback((id, value, isValid) => {}, [])
-
-  const descriptionInputHandler = useCallback((id, value, isValid) => {}, [])
+  const inputHandler = useCallback((id, value, isValid) => {
+    dispatch({type: 'INPUT_CHANGE', inputId: id, isValid, value})
+  }, [])
 
   return (
     <form className='place-form'>
       <Input
         id='title'
-        onInput={titleInputHandler}
+        onInput={inputHandler}
         type='text'
         label='Title'
         element={'input'}
@@ -25,12 +62,15 @@ function NewPlace() {
       />
       <Input
         id='descrption'
-        onInput={descriptionInputHandler}
+        onInput={inputHandler}
         label='Description'
         element={'textarea'}
         validators={[VALIDATOR_MINLENGTH(5)]}
         errorText='please enter a valid description (at least 5 characters).'
       />
+      <Button type='submit' disabled={!formState.isValid}>
+        ADD PLACE
+      </Button>
     </form>
   )
 }
