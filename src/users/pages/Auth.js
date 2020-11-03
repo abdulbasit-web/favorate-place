a0d4ccc221ee0b3269a9764c1d72428e92d9fe3d
abdulbasit-web <abdullbasit99salah@gmail.com>
2020-11-03 18:23:49 +0300

    signup with image

--- src/users/pages/Auth.js
+++ src/users/pages/Auth.js
@@ -83,17 +83,15 @@ function Auth() {
       } catch (err) {}
     } else {
       try {
+        const formData = new FormData()
+        formData.append('name', formState.inputs.name.value)
+        formData.append('email', formState.inputs.email.value)
+        formData.append('password', formState.inputs.password.value)
+        formData.append('image', formState.inputs.image.value)
         const responsData = await sendRequest(
           'http://localhost:5000/api/users/signup',
           'POST',
-          JSON.stringify({
-            name: formState.inputs.name.value,
-            email: formState.inputs.email.value,
-            password: formState.inputs.password.value,
-          }),
-          {
-            'Content-Type': 'application/json',
-          }
+          formData
         )
 
         auth.login(responsData.user.id)
